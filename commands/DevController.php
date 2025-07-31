a07c7f8ab7774adbf4405063d66ef01e1147ddca
Yaroslav Hlina <glina@intelico.su>
2025-07-31 10:42:22 +0300

    fix dev controller

--- commands/DevController.php
+++ commands/DevController.php
@@ -3,6 +3,8 @@
 namespace app\commands;
 
 use src\Entity\Issuer\PayerIdentificationNumber;
+use src\Entity\Share\ShareRegisterNumber;
+use src\Integration\Bcse\ShareInfo\BcseShareInfoFetcher;
 use src\Integration\CentralDepo\CentralDepoIssuerAndShareInfoFetcher;
 use src\Integration\Egr\Address\EgrAddressFetcher;
 use yii\console\Controller;
@@ -15,6 +17,7 @@ class DevController extends Controller
         $module,
         private CentralDepoIssuerAndShareInfoFetcher $fetcher,
         private EgrAddressFetcher $egrAddressFetcher,
+        private BcseShareInfoFetcher $bcseShareInfoFetcher,
         $config = [],
     ) {
         parent::__construct($id, $module, $config);
@@ -31,6 +34,17 @@ class DevController extends Controller
     public function actionEgr(string $pid): int
     {
         $dto = $this->egrAddressFetcher->get(new PayerIdentificationNumber($pid));
+        print_r($dto);
+
+        return ExitCode::OK;
+    }
+
+    public function actionBcse(string $pid, string $shareRegisterNumber): int
+    {
+        $dto = $this->bcseShareInfoFetcher->get(
+            new PayerIdentificationNumber($pid),
+            new ShareRegisterNumber($shareRegisterNumber),
+        );
         print_r($dto);
 
         return ExitCode::OK;
