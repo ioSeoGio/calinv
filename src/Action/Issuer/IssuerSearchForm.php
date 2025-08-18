a993b21fb0fa9eaa064daa8770c5de24069419dc
Yaroslav Hlina <glina@intelico.su>
2025-08-18 10:55:26 +0300

    fix issuer

--- src/Action/Issuer/IssuerSearchForm.php
+++ src/Action/Issuer/IssuerSearchForm.php
@@ -22,8 +22,8 @@ class IssuerSearchForm extends Model
     {
         $query = Issuer::find()
             ->with(['shares', 'businessReputationInfo'])
-            ->andWhere(['!=', 'pid', null])
-            ->andWhere(['!=', 'pid', ''])
+            ->andWhere(['!=', '_pid', null])
+            ->andWhere(['!=', '_pid', ''])
             ->addOrderBy([Issuer::tableName() . '.name' => SORT_ASC]);
 
         $dataProvider = new ActiveDataProvider([
