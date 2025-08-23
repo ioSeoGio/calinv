6ed10949c368678f6e8f64cd34ef78ef2ca5add6
Yaroslav Hlina <glina@intelico.su>
2025-08-23 16:30:50 +0300

    fix profit chart

--- views/portfolio/charts.php
+++ views/portfolio/charts.php
@@ -7,50 +7,81 @@ use yii\helpers\ArrayHelper;
 
 /** @var \yii\data\ActiveDataProvider $dataProvider */
 
-echo $this->render('tabs', []);
+?>
+<?= $this->render('tabs', []); ?>
+<div class="row justify-content-center">
 
-$data = ArrayHelper::map(
+    <?php $data = ArrayHelper::map(
         $dataProvider->query->all(),
         'id',
         function (PersonalShare $personalShare) {
             return [
-                'name' => $personalShare->share->getFormattedNameWithIssuer(),
-                'y' => $personalShare->getTotalCurrentPriceSum(),
-                'z' => $personalShare->getTotalProfitSum(),
+                    'name' => $personalShare->share->getFormattedNameWithIssuer(),
+                    'y' => $personalShare->getTotalCurrentPriceSum(),
+                    'z' => $personalShare->getTotalProfitSum(),
             ];
         },
     );
-$data = array_values($data);
-echo DynamicPieChartWidget::widget([
-    'data' => $data,
-    'title' => 'Доли портфеля в рублях',
-    'pointFormat' => '
-        <span style="color:{point.color}"></span> <b>{point.name}</b><br/>По текущей цене: <b>{point.y} р.</b><br/>По прибыли: <b>{point.z} р.</b><br/>
-    ',
-]);
+    $data = array_values($data); ?>
+    <div class="col-12 col-xxl-6">
+        <?= DynamicPieChartWidget::widget([
+                'data' => $data,
+                'title' => 'Доли портфеля по текущей цене акций в рублях. Чем шире кусок - тем больше доля по текущей цене. Чем жирнее - тем больше доля прибыли',
+                'pointFormat' => '
+            <span style="color:{point.color}"></span> <b>{point.name}</b><br/>Сумма в портфеле по текущей цене акции: <b>{point.y} р.</b><br/>Всего прибыли: <b>{point.z} р.</b><br/>
+        ',
+        ]); ?>
+    </div>
 
-$data = ArrayHelper::map(
-        $dataProvider->query
-            ->andWhere(['IS NOT', 'share.currentPrice', null])
-            ->andWhere('share."currentPrice" >= "buyPrice"')
-            ->all(),
+    <?php $data = ArrayHelper::map(
+        $dataProvider->query->all(),
         'id',
         function (PersonalShare $personalShare) {
             return [
-                'name' => $personalShare->share->getFormattedNameWithIssuer(),
-                'y' => $personalShare->getTotalProfitSum(),
-                'z' => 1,
+                    'name' => $personalShare->share->getFormattedNameWithIssuer(),
+                    'y' => $personalShare->getTotalBoughtSum(),
+                    'z' => $personalShare->getTotalProfitSum(),
             ];
         },
     );
-$data = array_values($data);
-echo "<hr>";
-echo DynamicPieChartWidget::widget([
-    'data' => $data,
-    'title' => 'Доли прибыли в портфеле по акциям в рублях. Убыточные в графике не отображаются <br>Всего '
-        . SimpleNumberFormatter::toView(Yii::$app->user->identity->getTotalProfit())
-        . ' р. прибыли',
-    'pointFormat' => '
-        <span style="color:{point.color}"></span> <b>{point.name}</b><br/>Прибыль: <b>{point.y} р.</b><br/>
-    ',
-]);
+    $data = array_values($data); ?>
+    <div class="col-12 col-xxl-6">
+        <?= DynamicPieChartWidget::widget([
+                'data' => $data,
+                'title' => 'Доли портфеля по закупочной цене акций в рублях. Чем шире кусок - тем больше доля по закупочной цене. Чем жирнее - тем больше доля прибыли',
+                'pointFormat' => '
+            <span style="color:{point.color}"></span> <b>{point.name}</b><br/>Сумма в портфеле по закупочной цене акции: <b>{point.y} р.</b><br/>Всего прибыли: <b>{point.z} р.</b><br/>
+        ',
+        ]); ?>
+    </div>
+
+    <?php $data = ArrayHelper::map(
+        $dataProvider->query
+                ->andWhere(['IS NOT', 'share.currentPrice', null])
+                ->andWhere('share."currentPrice" >= "buyPrice"')
+                ->all(),
+        'id',
+        function (PersonalShare $personalShare) {
+            return [
+                    'name' => $personalShare->share->getFormattedNameWithIssuer(),
+                    'y' => $personalShare->getTotalProfitSum(),
+                    'z' => 1,
+            ];
+        },
+    );
+    $data = array_values($data);
+    ?>
+
+    <hr>
+    <div class="col-12 col-xxl-6">
+        <?= DynamicPieChartWidget::widget([
+                'data' => $data,
+                'title' => 'Доли прибыли в портфеле по акциям в рублях. Убыточные в графике не отображаются <br>Всего '
+                        . SimpleNumberFormatter::toView(Yii::$app->user->identity->getTotalProfit())
+                        . ' р. прибыли',
+                'pointFormat' => '
+            <span style="color:{point.color}"></span> <b>{point.name}</b><br/>Прибыль: <b>{point.y} р.</b><br/>
+        ',
+        ]); ?>
+    </div>
+</div>
